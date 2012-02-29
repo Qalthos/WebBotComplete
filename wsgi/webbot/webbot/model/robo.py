c514f23726c15f3154c84dc9e17250e8220d3cd3
Nathaniel Case <Qalthos@gmail.com>
2012-02-28 22:26:57 -0500

    new model

---
+++ wsgi/webbot/webbot/model/robo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""My Login Modules stuff."""
+
+from sqlalchemy import *
+from sqlalchemy.orm import mapper, relation
+from sqlalchemy import Table, ForeignKey, Column
+from sqlalchemy.types import Integer, Unicode, DateTime
+#from sqlalchemy.orm import relation, backref
+
+from datetime import datetime
+
+from webbot.model import DeclarativeBase, metadata, DBSession
+
+class Game(DeclarativeBase):
+    __tablename__ = 'games'
+
+    id = Column(Unicode(255), primary_key=True)
+    username = Column(Unicode(255), nullable=False)
+    date = Column(DateTime, nullable=True)
