759355cedea5faad6fe8fa8152000a5f832c8f43
Nathaniel Case <Qalthos@gmail.com>
2012-03-08 15:54:40 -0800

    asldkfja

--- wsgi/webbot/webbot/public/javascript/auth-fb.js
+++ wsgi/webbot/webbot/public/javascript/auth-fb.js
@@ -66,7 +66,7 @@
   handle_page = function() {
       $('#login > a').text("Hello, "+globals.data['name']);
       $(':input:hidden').val(globals.data['id']);
-      $('.uid').href($(this).href+globals.data['id']);
+      $('.uid').attr('href', $(this).attr('href')+globals.data['id']);
   };
 
 
